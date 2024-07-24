4414220b5f420f36f5f3f4451771cd7660544f65
Arif Miftahul Yusuf <rfmiftahul@gmail.com>
2024-07-24 20:17:57 +0700

    Tambah function SumInt

--- get_hello_world.go
+++ get_hello_world.go
@@ -3,3 +3,12 @@ package go_say_helo
 func GetHelloWorld() string {
 	return "Hello worlds"
 }
+
+func SumInt(number ...int) int {
+	var result int
+	for _, v := range number {
+		result += v
+	}
+
+	return result
+}
